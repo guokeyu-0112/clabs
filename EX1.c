619e5862ecce343303ec2675328581a6a9a7c632
guokeyu-0112 <2897868864@qq.com>
2019-12-26 02:05:45 -0800

    lab3

--- EX1.c
+++ EX1.c
@@ -1,38 +1,55 @@
-#include <stdio.h>
+
+#include<stdio.h>
+#include<math.h>
+int power(int base, int n){
+    int i,p;
+    p=1;
+    for (i=1;i<=n;++i)
+    p = p*base;
+    return p;
+}
 int main()
 {
-    char input_char;
-    int dec[6];
-int a,b,k,input,count;
-a=0,b=0,input=0,count=0;
-int bin[17];
-input_char=gitchar();
-while(input_char!='\n')
-{
-    dec[a]=input_char-'0';
-    a++;
-}
-a--;
-for( ;a>=0;a--)
-{
-    for(k=0;k<b;k++)
-    {
-        dec[a]=dec[a]*10;
+    char number1[18] = {0};
+    int i = 0;
+    char number;
+    int sum = 0;
+    int gen =-1;
+    unsigned int b = 1;
+    printf("please input a positive hexadecimal number(start with 0X/0x)\n");
+    while((number = getchar()) != '\n'){
+        number1[i]=number;
+        i++;
+        gen++;
     }
-    input=input+dec[a];
-    b++;
-}
-for(a=0;input>0;a++)
-{
-    bin[a]=((input&1)!=0?1:0);
-    input>>1;
-}
-a--;
-for( ;a>=0;a--)
-{
-    printf("%d",bin[a]);
-    if(bin[a]==1)
-    count++;
-}
-printf("the number of 1 is %d",count);
+        if(number1[0]!='0'){
+        printf("input error\n");
+        return 0;}
+        if(number1[1]!='x'&&number1[1]!='X'){
+            printf("input error\n");
+            return 0;
+        }
+    i--;
+    while(i > 1)
+    {
+            if(number1[i]<='9'&&number1[i]>='0')
+                sum+=(number1[i]-'0')*power(16,gen-i);
+            else if(number1[i]>='A'&&number1[i]<='F')
+                sum+=(number1[i]-'A'+10)*power(16,gen-i);
+            else if(number1[i]>='a'&&number1[i]<='f')
+            sum+=(number1[i]+'A'-'a'-'A'+10)*power(16,gen-i);
+            else{
+                printf("input error\n");
+                return 0;
+                }
+            i--;
+    }
+    while(b!=0){
+        b=b<<1;
+    }
+    if(sum>b-1){
+    printf("the max is %u,your input number  is too big\n",b-1);
+    }
+    else printf("%d\n",sum);
+    return 0;
 }
